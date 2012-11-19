109a4922bb6a6b65890f5c257822b1d610056d1b
Steve Hickey <steve.hickey@freshtilledsoil.com>
2012-11-19 11:54:58 -0500

    Update with latest version of gridOverlay plugin

--- 06-plus-baseline-grid/js/gridOverlay.js
+++ 06-plus-baseline-grid/js/gridOverlay.js
@@ -14,42 +14,73 @@
   "use strict";
 
   var options = {
-    'pageWidth' :      '1000px',                      // set page width (added as max-width for flexible grids)
-    'columnWidth' :    '150px',                       // set column width
-    'columnColor' :    'rgba(255, 0, 0, 0.25)',       // set column color
-    'gutterWidth' :    '20px',                        // set gutter width
-    'gutterColor' :    'transparent',                 // set gutter color
-    'widthUnits' :     'px',                          // set measurement units for column/gutter widths
-    'baselineHeight' : '22px',                        // set baseline height
-    'baselineColor' :  'rgba(255, 255, 255, 1)',      // set baseline color
-    'initialDisplay' : 'none'                         // use 'block' to have grid visible by default
+    'maxPageWidth' :     '1000px',                      // set overlay width to match page width (as max-width for flexible grids)
+    'columnWidth' :      '150px',                       // set column width in px, em or %
+    'columnCount' :       6,                            // set column count
+    'columnColor' :      'rgba(255, 0, 0, 0.25)',       // set column color
+    'gutterWidth' :      '20px',                        // set gutter width in px, em or %
+    'baselineHeight' :    22,                           // set baseline height (requires pixels for now)
+    'baselineColor' :    'rgba(255, 255, 255, 1)',      // set baseline color
+    'initialDisplay' :   'none'                         // use 'block' to have grid visible by default
   },
-  columnGradient = options.columnColor + ' 0, ' + options.columnColor + ' ' + parseInt(options.columnWidth, 10) + 'px, ' + options.gutterColor + ' ' + parseInt(options.columnWidth, 10) + 'px',
-  baselineGradient = ( parseInt(options.baselineHeight, 10) - 1 ) + 'px, ' + options.baselineColor + ' ' + parseInt(options.baselineHeight, 10) + 'px',
-  grid = document.createElement('div');
 
-  grid.id = 'gridOverlay';
-  grid.style.cssText = '\
+  // parse integers from option variables
+  maxPageWidthInt = parseInt(options.maxPageWidth, 10),
+  columnWidthInt = parseInt(options.columnWidth, 10),
+  gutterWidthInt = parseInt(options.gutterWidth, 10),
+
+  // calculate column and gutter widths in percentages
+  columnPercentage = ( columnWidthInt / maxPageWidthInt ) * 100 + '%',
+  gutterPercentage = ( gutterWidthInt / maxPageWidthInt ) * 100 + '%',
+
+  // create baseline grid gradient
+  baselineGradient = ( options.baselineHeight - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baselineHeight + 'px',
+
+  // create gridOverlay element
+  gridOverlay = document.createElement('div');
+
+  // assign id to gridOverlay element
+  gridOverlay.id = 'gridOverlay';
+
+  // style gridOverlay element (max-width is overwritten manually for tutorial to compensate for body padding. padding and border box added for same reason)
+  gridOverlay.style.cssText = '\
     display: ' + options.initialDisplay + '; \
-    position: absolute; top: 0; right: 0; bottom: 0; left: 0; margin: 0 auto; width: 100%; \
-    max-width: ' + parseInt(options.pageWidth, 10) + 'px; \
-    background-size: ' + ( parseInt(options.columnWidth, 10) + parseInt(options.gutterWidth, 10) ) + options.widthUnits + ' ' + parseInt(options.baselineHeight, 10) + 'px; \
-    background-image: -webkit-linear-gradient(left, ' + columnGradient + '), -webkit-linear-gradient(top, transparent ' + baselineGradient + '); \
-    background-image:    -moz-linear-gradient(left, ' + columnGradient + '),    -moz-linear-gradient(top, transparent ' + baselineGradient + '); \
-    background-image:     -ms-linear-gradient(left, ' + columnGradient + '),     -ms-linear-gradient(top, transparent ' + baselineGradient + '); \
-    background-image:      -o-linear-gradient(left, ' + columnGradient + '),      -o-linear-gradient(top, transparent ' + baselineGradient + '); \
-    background-image:                 linear, left, ' + columnGradient + ',                  linear, top, transparent ' + baselineGradient + ');';
+    position: absolute; top: 0; right: 0; bottom: 0; left: 0; margin: 0 auto; width: 100%; padding: 0 1em; box-sizing: border-box; \
+    max-width: 1032px; \
+    background-size: 100% ' + options.baselineHeight + 'px; \
+    background-image: -webkit-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:    -moz-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:     -ms-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:      -o-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:                 linear, top, transparent ' + baselineGradient + ');';
 
-  document.body.appendChild(grid);
+  // add grid container to page
+  document.body.appendChild(gridOverlay);
+
+  // add columns to page and style them
+  for (var i = 0; i < options.columnCount; i++) {
+    var column = document.createElement('div');
+    gridOverlay.appendChild(column);
+    column.style.cssText = '\
+      display: block; float: left; position: relative; height: auto; min-height: 100%; \
+      background-color: ' + options.columnColor + '; \
+      margin-right: ' + gutterPercentage + '; \
+      width: ' + columnPercentage + ';';
+  };
+
+  // remove right margin from last column
+  gridOverlay.lastChild.style.marginRight = 0;
+
+  // style body to prevent scrolling grid and match full page height
   document.body.style.cssText = 'position: relative; height: auto; min-height: 100%';
 
+  // enable toggling of grid visibility with Option + G (Mac) or Alt + G (PC)
   document.addEventListener('keyup', function (e) {
     if (e.keyCode === 71 && e.altKey) {
-      var grid = document.getElementById('gridOverlay');
-      if (grid.style.display === 'none') {
-        grid.style.display = 'block';
+      if (gridOverlay.style.display === 'none') {
+        gridOverlay.style.display = 'block';
       } else {
-        grid.style.display = 'none';
+        gridOverlay.style.display = 'none';
       }
     }
   });
